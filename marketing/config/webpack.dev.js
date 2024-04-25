136ce77e79959ba30c6f3bba40aae966c21333b5
vladik-Didyk <62429920+vladik-Didyk@users.noreply.github.com>
2024-04-25 11:46:41 -0400

    Fix typo

--- marketing/config/webpack.dev.js
+++ marketing/config/webpack.dev.js
@@ -1,17 +1,18 @@
-const { merge } = require("webpack-merge");
-const HtmlWbepackPlugin = require("html-webpack-plugin");
-const commonConfig = require("./webpack.common");
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const commonConfig = require('./webpack.common');
 
 const devConfig = {
+  mode: 'development',
   devServer: {
     port: 8081,
     historyApiFallback: {
-      index: "index.html",
+      index: 'index.html',
     },
   },
   plugins: [
-    new HtmlWbepackPlugin({
-      template: "./public/index.html",
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
     }),
   ],
 };
