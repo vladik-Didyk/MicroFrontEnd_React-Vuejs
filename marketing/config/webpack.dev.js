974599dbdfb3ce92b97f198bd0e61a912b33a6a3
vladik-Didyk <62429920+vladik-Didyk@users.noreply.github.com>
2024-04-25 12:21:02 -0400

    42. Integration of the Container and Marketing

--- marketing/config/webpack.dev.js
+++ marketing/config/webpack.dev.js
@@ -1,18 +1,26 @@
-const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const commonConfig = require('./webpack.common');
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
 
 const devConfig = {
-  mode: 'development',
+  mode: "development",
   devServer: {
     port: 8081,
     historyApiFallback: {
-      index: 'index.html',
+      index: "index.html",
     },
   },
   plugins: [
+    new ModuleFederationPlugin({
+      name: "marketing",
+      filename: "remotyEntry.js",
+      exposes: {
+        "./MarketingApp": "./src/bootstrap",
+      },
+    }),
     new HtmlWebpackPlugin({
-      template: './public/index.html',
+      template: "./public/index.html",
     }),
   ],
 };
