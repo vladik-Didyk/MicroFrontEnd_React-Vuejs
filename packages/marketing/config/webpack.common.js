0d6e289eba84febe49d47c08f989c05ae0373df4
vladik-Didyk <62429920+vladik-Didyk@users.noreply.github.com>
2024-04-25 14:58:04 -0400

    fix typo

--- packages/marketing/config/webpack.common.js
+++ packages/marketing/config/webpack.common.js
@@ -1,6 +1,3 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { plugins } = require("../../container/config/webpack.common");
-
 module.exports = {
   module: {
     rules: [
@@ -8,18 +5,13 @@ module.exports = {
         test: /\.m?js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-react", "@babel/preset-env"],
-            plugins: ["@babel/plugin-transform-runtime"],
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime'],
           },
         },
       },
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
-  ],
 };
