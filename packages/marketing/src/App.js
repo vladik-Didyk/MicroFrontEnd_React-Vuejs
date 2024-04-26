4ca7a5be0dc8db47fd12565835761f3a557a0a88
vladik-Didyk <62429920+vladik-Didyk@users.noreply.github.com>
2024-04-26 13:24:13 -0400

    92. Communicating Through Callbacks

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { Switch, Route, Router } from "react-router-dom";
+import React from 'react';
+import { Switch, Route, Router } from 'react-router-dom';
 import {
   StylesProvider,
   createGenerateClassName,
-} from "@material-ui/core/styles";
+} from '@material-ui/core/styles';
 
-import Landing from "./components/Landing";
-import Pricing from "./components/Pricing";
+import Landing from './components/Landing';
+import Pricing from './components/Pricing';
 
 const generateClassName = createGenerateClassName({
-  productionPrefix: "ma",
+  productionPrefix: 'ma',
 });
 
 export default ({ history }) => {
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,18 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createMemoryHistory } from 'history';
+import App from './App';
 
 // Mount function to start up the app
-const mount = (el) => {
+const mount = (el, { onNavigate }) => {
   const history = createMemoryHistory();
+
+  history.listen(onNavigate);
 
   ReactDOM.render(<App history={history} />, el);
 };
 
 // If we are in development and in isolation,
 // call mount immediately
-if (process.env.NODE_ENV === "development") {
-  const devRoot = document.querySelector("#_marketing-dev-root");
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_marketing-dev-root');
 
   if (devRoot) {
     mount(devRoot);
