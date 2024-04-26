8891e3a36367fba2c111edc61b44529b475fba1b
vladik-Didyk <62429920+vladik-Didyk@users.noreply.github.com>
2024-04-26 11:20:06 -0400

    Add header to container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import MarketingApp from './components/MarketingApp';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
 
 export default () => {
   return (
+<<<<<<< Updated upstream
     <div>
       <h1>Hi there!</h1>
       <hr />
       <MarketingApp />
     </div>
+=======
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
+>>>>>>> Stashed changes
   );
 };
