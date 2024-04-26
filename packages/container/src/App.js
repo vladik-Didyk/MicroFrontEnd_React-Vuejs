b866bf12b351fd2efadef01b91a5489d4213a9db
vladik-Didyk <62429920+vladik-Didyk@users.noreply.github.com>
2024-04-26 11:25:45 -0400

    Add header to container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,23 +1,26 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import MarketingApp from "./components/MarketingApp";
-import Header from "./components/Header";
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
+
+import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
 
 export default () => {
   return (
-<<<<<<< Updated upstream
-    <div>
-      <h1>Hi there!</h1>
-      <hr />
-      <MarketingApp />
-    </div>
-=======
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
->>>>>>> Stashed changes
   );
 };
