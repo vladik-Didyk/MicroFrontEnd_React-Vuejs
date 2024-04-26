f1372f20fa92f6598fc9d3c5bc2273adf4e6e532
vladik-Didyk <62429920+vladik-Didyk@users.noreply.github.com>
2024-04-26 11:33:40 -0400

    Remove issued lines

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -5,19 +5,11 @@ import Header from "./components/Header";
 
 export default () => {
   return (
-<<<<<<< Updated upstream
-    <div>
-      <h1>Hi there!</h1>
-      <hr />
-      <MarketingApp />
-    </div>
-=======
     <BrowserRouter>
       <div>
         <Header />
         <MarketingApp />
       </div>
     </BrowserRouter>
->>>>>>> Stashed changes
   );
 };
